49254c5f4c9e186a3bf99cd68aee20018d7b752a
amp3193 <amp3193@yahoo.com>
2019-05-16 21:51:05 -0400

    skeletons and todos on js files. Html done for survey

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+const friends = [{
+    "name":"Abed",
+    "photo":"https://static.comicvine.com/uploads/original/8/83918/2524197-abed.png",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+];
+  
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
